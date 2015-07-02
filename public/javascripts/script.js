a428c2dffd90bf076b4a4348a6b95be98c81a773
bonnieso <so_bonnie@hotmail.com>
2015-07-02 01:48:02 -0700

    updating with firebase

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,40 +1,91 @@
 var bmi = angular.module("bmiApp", []);
 
-  bmi.factory('foodFactory', function(){
-    var fn = {};
-    fn.foodStorage = [];
-  
-    fn.storeFood = function(newFood){
-      fn.foods.unshift(newFood);
-    }
-
-    fn.deleteFood = function(foodIndex){
-      fn.foods.splice(foodIndex, 1);
-    }
-
-
-    fn.updateFood = function(foodIndex, currentFood){
-
-      fn.foods.splice(foodIndex, 1, {item: currentFood});
-    }
-  
-    return fn;
-})
-  
 var foodData = [];
 
 bmi.controller("bmiCtrl", function($scope, $http) {
   $scope.foodArray = [];
   $scope.date = new Date();
   
-//  $scope.addFood = function(food) {
-//    food.date = $scope.date;
-//    $scope.foodArray.push(food);
-//    $scope.food = "";
-//    console.log($scope.foodArray);
-//  };
+  //initialize page with database info
+  function getUsers(){
+    $http.get("/users")
+      .success(function(data) {
+        $scope.userDisplay = data.user;
+        $scope.user = "";
+        $scope.stats = {
+          name: data.user.name,
+          bmi: computeBMI(data.user),
+          oldWeight: data.user.weight,
+          weightChange: computeWeight($scope.foodArray),
+          currentWeight: +data.user.weight + computeWeight($scope.foodArray)
+        };
+     //      console.log($scope.foodArray[1]);
+    }).catch(function(err) {
+        console.log(err);
+    });
+  }
+  
+  getUsers();
+  //
+  function getFoods(){
+    $http.get("/foods")
+      .success(function(data) {
+
+      for(var key in data.foods){
+        data.foods[key].refKey = key;
+        $scope.foodArray.push(data.foods[key]);
+        
+      }
+//      console.log($scope.foodArray);
+      $scope.food = "";
+
+    }).catch(function(err) {
+      console.log(err);
+    });
+  }
+  
+  getFoods();
+  //
+  
+  function computeWeight(foods){
+    console.log(foods);
+    var totalCalories = foods.reduce(function(acc, foods){
+      return acc + (foods.calories * foods.servings);
+    }, 0);
+    return totalCalories/3500;
+  }
+  
+  function computeBMI(user){
+    var inches = user.unit === "in" ? +user.height : +user.height * 0.393701;
+    return (+user.weight * 703)/Math.pow(inches, 2);
+  }
+  
+  //adding food
+  $scope.addFood = function(food) {
+    food.date = $scope.date;
+    $http.post("/foods", food)
+      .success(function(data) {
+        $scope.foodArray.push(data);
+//      $scope.food = "";
+//      console.log("post data", data)
+    }).catch(function(err) {
+      console.log(err);
+    });
+    
+//    getFoods();
+//    $scope.foodArray.push(data)
+  };
   
   $scope.deleteFood = function($index){
+//    console.log($scope.foodArray[$index].refKey);
+
+    $http.delete("/foods/"+$scope.foodArray[$index].refKey)
+      .success(function(data){ 
+        
+      }).catch(function(err){
+      console.log(err);
+    });
+      
     $scope.foodArray.splice($index, 1);
   };
   
@@ -43,43 +94,18 @@ bmi.controller("bmiCtrl", function($scope, $http) {
     console.log($scope.foodArray[index])
   };
   
-  
-  //
-  $scope.addFood = function(foodObj){
-    $http.post("/foods", foodObj)
-      .success(function(data){
-    });
-//    $http.get
-  };
-  
-  
-  $scope.getFoods = function(){
-    $http.get("/foods").success(function(response){
-      var fArray = response.split(",");
-      fArray.pop();
-      console.log(fArray);
-//      $scope.foodArray = response;
-    });
-
-  };
-//  $scope.addFood = function(food) {
-//    $scope.formError = "";
-//    $http.post("/foods", { foodItem: $scope.food}).success(function(data) {
-//      foodData.push($scope.food);
-//      $scope.food = "";
-//    }).catch(function(err) {
-//      $scope.formError = err.data.error;
-//    });
-//  };
-  
-  $scope.userArray = [];
+  // add user stuff (I'm done here)
   $scope.addUser = function(user) {
-    $scope.userArray.push(user);
-    $scope.user = "";    
-  };
-  
-  $scope.deleteUser = function($index){
-    $scope.userArray.splice($index, 1);
+    
+    $http.post("/users", user)
+      .success(function(data) {
+        //w00t! success!
+    }).catch(function(err) {
+      console.log(err);
+    });
+    
+getUsers();
+    
   };
 
 });
--- routes/index.js
+++ routes/index.js
@@ -1,30 +1,54 @@
-var express = require('express'), fs = require("fs");
+var express = require('express'), fs = require("fs"), Firebase = require("firebase");
 var router = express.Router();
+
+var bmiRef = new Firebase("https://bmi-food-diary.firebaseio.com/");
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Food Diary' });
 });
 
-router.get('/foods', function(req, res, next) {
-  fs.readFile("foods.json", "utf8", function(err, data){
-    if(err){
-      throw err;
-    }
-    res.json(data);
-  })
+router.get('/users', function(req, res, next) {
+  
+  bmiRef.child("user").once("value", function(snap) {
+    res.json( { user: snap.val() } );
+  });
+  
 });
 
 
-router.post('/foods', function(req, res, next) {
+router.post('/users', function(req, res, next) {
   
-  console.log(req.body);
-  fs.appendFile("foods.json", JSON.stringify(req.body)+",", function(err){
-    if (err){
-      throw err;
-    }
+  bmiRef.child("user").set(req.body, function(err) {
     res.json(req.body);
   });
+  
+});
+
+//
+
+router.get('/foods', function(req, res, next) {
+  
+  bmiRef.child("food").once("value", function(snap) {
+    console.log(snap.val());
+    res.json( { foods: snap.val() } );
+  });
+  
+});
+
+router.post('/foods', function(req, res, next) {
+  
+  bmiRef.child("food").push(req.body, function(err) {
+    res.json(req.body);
+  });
+  
+});
+
+router.delete('/foods/:key', function(req, res, next) {
+  bmiRef.child("food").child(req.params.key).remove(function(err) {
+    res.json(req.body);
+  });
+  
 });
 
 module.exports = router;
